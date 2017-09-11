a1c60dbca293e3efb9b8784790f5ae368febc2e4
KayleneRoroihape RamekaRirinui <10006663@BCS.NET.NZ>
2017-09-12 11:36:37 +1200

    Created MVC Project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Assessment2.Models;
+
+namespace Assessment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            ViewData["Message"] = "Your application description page.";
+            ViewData["Title"] = "Profile";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+            ViewData["Title"] = "Contact";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
